fae72fa841acd232009f369535cc991c6f2160f2
chenyinheng30 <chenyinheng30@Outlook.com>
2019-03-16 18:22:28 +0800

    添加.gitignore;创建文件结构和src/physic-bottom.c，src/space-main.c;增加科学计数法

---
+++ src/physic-bottom.c
@@ -0,0 +1,10 @@
+/*
+ *文件名:physic-bottom.c
+ *描述:行星轨道有关的计算，定义基本的物理量
+ */
+//科学计数法
+struct S-notstion{
+	double smul;
+	int power;
+	//suml*10^power
+}
---
+++ src/space-main.c
@@ -0,0 +1,12 @@
+/*
+ *文件名:space-main.c
+ *描述:游戏Space(待定)的入口
+ *作者:chenyinheng30,及:
+ *开始时间:2019-5-3
+ *结束时间:
+ */
+int main()
+{
+
+	return 0;
+}
